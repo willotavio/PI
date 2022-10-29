ceb2ccf3549d1d6010b859e2e16584c1df3601b2
willotavio <willaotavio@gmail.com>
2022-10-29 19:30:14 -0300

    Melhorias no sistema de troca de temas

--- js/trocaTema.js
+++ js/trocaTema.js
@@ -1,11 +1,30 @@
-function colormode(toggle) {
-    document.querySelector('.navbar').style.backgroundColor = (toggle) ? 'black':'#0478d9';
-    document.querySelector('footer').style.backgroundColor = (toggle) ? 'black':'#0478d9';
-};
+const trocaTema = document.querySelector(".trocaTema");
 
-var toggled = true;
-const switcher = document.querySelector('.trocaTema');
-switcher.addEventListener('click',function() {
-   colormode(toggled);
-   toggled = !toggled;
-});
+var light = document.getElementsByClassName('light');
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme == "dark") {
+    for( var i = 0; i < light.length; i++){
+    document.getElementsByClassName('light')[i].classList.add("dark");
+    }
+  }
+
+trocaTema.addEventListener("click", function(){
+    for( var i = 0; i < light.length; i++){
+        document.getElementsByClassName('light')[i].classList.toggle("dark");
+    }
+    var className = document.body.className;
+    if(className == "light") {
+        this.textContent = "Light";
+    }
+    else {
+        this.textContent = "Dark";
+    }
+
+    let theme = "light";
+  if (document.body.classList.contains("dark")) {
+    theme = "dark";
+}
+  localStorage.setItem("theme", theme);
+})
